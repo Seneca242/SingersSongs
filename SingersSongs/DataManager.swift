71411e10e214c2be94e30c35b34839844abb23b5
Seneca242 <flabbergasted242@gmail.com>
2023-07-02 23:29:26 +0300

    made link Singer-Songs without songs editing

--- SingersSongs/DataManager.swift
+++ SingersSongs/DataManager.swift
@@ -26,6 +26,48 @@ class DataManager {
     private init(){
         viewContext = persistentContainer.viewContext
     }
+    
+    func createSinger(name: String) -> Singer {
+        let singer = Singer(context: viewContext)
+        singer.name = name
+        return singer
+    }
+    
+    func createSong(title: String, realeseData: String, singer: Singer) -> Song {
+        let song = Song(context: viewContext)
+        song.title = title
+        song.releaseDate = realeseData
+        song.singer = singer
+        return song
+    }
+    
+    func fetchSingers() -> [Singer] {
+        let request: NSFetchRequest<Singer> = Singer.fetchRequest()
+        var fetchedSingers: [Singer] = []
+        
+        do {
+            fetchedSingers = try viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching singers \(error)")
+        }
+        return fetchedSingers
+    }
+    
+    func fetchSongs(singer: Singer) -> [Song] {
+        let request: NSFetchRequest<Song> = Song.fetchRequest()
+        request.predicate = NSPredicate(format: "singer = %@", singer)
+        request.sortDescriptors = [NSSortDescriptor(key: "releaseDate", ascending: false)]
+        var fetchedSongs: [Song] = []
+        
+        do {
+            fetchedSongs = try viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching songs \(error)")
+        }
+        return fetchedSongs
+    }
+    
+    
 
     // MARK: - Core Data Saving support
 
--- SingersSongs/SingersTableViewController.swift
+++ SingersSongs/SingersTableViewController.swift
@@ -8,82 +8,96 @@
 import UIKit
 
 class SingersTableViewController: UITableViewController {
+    
+    var singers = DataManager.shared.fetchSingers()
+    
+    private let cellID = "singerCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupNavigationBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
     }
+    
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        singers.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let singer = singers[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = singer.name
+        cell.accessoryType = .disclosureIndicator
+        cell.contentConfiguration = content
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let songVC = SongsTableViewController(index: indexPath.row)
+        navigationController?.pushViewController(songVC, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    private func setupNavigationBar() {
+        title = "Singers"
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.backgroundColor = UIColor(
+            red: 183/255,
+            green: 65/255,
+            blue: 14/255,
+            alpha: 1
+        )
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addSinger)
+        )
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    @objc private func addSinger(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(
+            title: "Enter new singer",
+            message: "",
+            preferredStyle: .alert
+        )
+        
+        let createAction = UIAlertAction(
+            title: "Create",
+            style: .default) { action in
+                let singer = DataManager.shared.createSinger(name: textField.text ?? "")
+                self.singers.append(singer)
+                DataManager.shared.saveContext()
+                self.tableView.reloadData()
+            }
+        
+        let cancelAction = UIAlertAction(
+            title: "Cancel",
+            style: .destructive) { action in
+                self.dismiss(animated: true)
+            }
+        
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Example: Beyonce"
+            textField = alertTextField
+        }
+        
+        alert.addAction(createAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
